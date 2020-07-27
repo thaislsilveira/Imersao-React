0446af1cc682612a356255891742646a4ffce3ea
peter-loliveira <peter.loliveira@gmail.com>
2020-07-27 16:58:32 -0300

    Inserido dois componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Menu from './components/Menu';
+import DadosIniciais from './data/dados_iniciais.json'
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
     <div style = {{background:"#414141"}}>
       <Menu/>
+      {/* <BannerMain/> */}
+      <Carousel
+        ignoreFirstVideo
+        category = {DadosIniciais.categorias[0]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category = {DadosIniciais.categorias[1]}
+      />
+      <Footer/>
     </div>
   );
 }
