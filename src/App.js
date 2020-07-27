29e7ccc7da2577911234f07bcfef9e1fb8354cea
peter-loliveira <peter.loliveira@gmail.com>
2020-07-27 17:10:53 -0300

    Primira páona pronta - Falta inserir lista pessoal

--- src/App.js
+++ src/App.js
@@ -9,16 +9,20 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div style = {{background:"#414141"}}>
-      <Menu/>
-      {/* <BannerMain/> */}
-      <Carousel
-        ignoreFirstVideo
-        category = {DadosIniciais.categorias[0]}
+    <div style={{ background: "#414141" }}>
+      <Menu />
+      <BannerMain
+        videoTitle={DadosIniciais.categorias[0].videos[0].titulo}
+        url = {DadosIniciais.categorias[0].videos[0].url}
+        videoDescription = {'O q  ue é o Front-End? Trabalhando na área'}
       />
       <Carousel
         ignoreFirstVideo
-        category = {DadosIniciais.categorias[1]}
+        category={DadosIniciais.categorias[0]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DadosIniciais.categorias[1]}
       />
       <Footer/>
     </div>
